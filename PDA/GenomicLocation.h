7de01ae7002aee4636db3978364003c129eca703
Brandon <baprice@andrew.cmu.edu>
2014-07-16 03:17:03 -0400

    added incomplete GenomicLocation and ReferenceGenome classes

---
+++ PDA/GenomicLocation.h
@@ -0,0 +1,51 @@
+#ifndef GENOMICLOCATION_H
+#define GENOMICLOCATION_H
+
+#include <string>
+#include "ReferenceGenome.h"
+
+class GenomicLocation
+{
+private:
+   int chrom_num;
+   uint64_t pos_num;
+   ReferenceGenome* ref_genome;
+   std::string rs_num;
+public:
+   GenomicLocation(int chrom, uint64_t pos, ReferenceGenome ref) : 
+      chrom_num(chrom), pos_num(pos) 
+   { 
+      ref_genome = &ref;
+   }
+   
+   GenomicLocation(int chrom, uint64_t pos) : 
+      chrom_num(chrom), pos_num(pos) 
+   { 
+      ref_genome = &(GENOME_HG19::Instance());
+   }
+   
+   GenomicLocation() : chrom_num(0), pos_num(0)
+   {
+      ref_genome = &(GENOME_HG19::Instance());
+   }
+
+   void setRSNumber(const std::string rs) {rs_num=rs;}
+
+   const int chromosome() const {return chrom_num;}
+
+   const int position() const {return pos_num;}
+
+   const std::string rsNumber() const {return rs_num;}
+};
+inline bool operator==(const GenomicLocation& lhs, const GenomicLocation& rhs)
+{
+   if (lhs.chromosome() == rhs.chromosome() && lhs.position() == rhs.position())
+      return true;
+   if ((lhs.rsNumber()).compare(rhs.rsNumber()) == 0)
+      return true;
+   if (/*TODO*/ false)
+      return true;
+   return false;
+}
+
+#endif
---
+++ PDA/ReferenceGenome.h
@@ -0,0 +1,46 @@
+#ifndef REFERENCEGENOME_H
+#define REFERENCEGENOME_H
+
+#include<string>
+
+class ReferenceGenome
+{
+protected:
+   ReferenceGenome(){}
+   ~ReferenceGenome(){}
+   ReferenceGenome(const ReferenceGenome&);        //Prevent copy-construction
+   ReferenceGenome& operator=(const ReferenceGenome&);    //Prevent assignment 
+   std::string ref_name;
+public:
+   static ReferenceGenome& Instance()
+   {
+      static ReferenceGenome singleton;
+      return singleton;
+   }
+
+   const std::string& name() const 
+   {
+      return ref_name;
+   }
+};
+inline bool operator==(const ReferenceGenome& lhs, const ReferenceGenome& rhs)
+{
+   return (((lhs.name()).compare(rhs.name())) == 0);
+}
+
+class GENOME_HG19 : public  ReferenceGenome
+{
+private:
+   GENOME_HG19(){}
+   ~GENOME_HG19(){}
+   GENOME_HG19(const GENOME_HG19&);
+   GENOME_HG19& operator=(const GENOME_HG19&);
+public:
+   static GENOME_HG19& Instance()
+   {
+      static GENOME_HG19 singleton;
+      return singleton;  
+   }
+};
+
+#endif
